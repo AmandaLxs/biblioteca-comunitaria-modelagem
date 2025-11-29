9eefc9f515a48b788fd7d89b0db8626a5f6227c3
Amanda Lima <amandalima.181@icloud.com>
2025-11-29 16:30:53 -0300

    V1 - Transactions and examples

--- sql/06_transactions_and_examples.sql
+++ sql/06_transactions_and_examples.sql
@@ -1,12 +1,23 @@
 -- 06_transactions_and_examples.sql
 USE biblioteca_comunitaria;
 
--- Exemplo: registrar empréstimo com verificação de disponibilidade (transação)
+-- Exemplo transacional: registrar novo empréstimo de forma segura
 START TRANSACTION;
--- verificar exemplar disponível
-SELECT id, disponivel FROM exemplar WHERE codigo_exemplar = 'EX-0004' FOR UPDATE;
--- se disponivel = TRUE então:
-INSERT INTO emprestimo (exemplar_id, usuario_id, data_emprestimo, data_prevista_devolucao)
-VALUES ( (SELECT id FROM exemplar WHERE codigo_exemplar='EX-0004'), (SELECT id FROM usuario WHERE cpf='33344455566'), CURDATE(), DATE_ADD(CURDATE(), INTERVAL 15 DAY) );
-UPDATE exemplar SET disponivel = FALSE WHERE codigo_exemplar = 'EX-0004';
+
+-- 1) Verifica exemplar disponível (FOR UPDATE trava linha)
+SELECT id, disponivel FROM exemplar WHERE codigo_exemplar = 'EX-0005' FOR UPDATE;
+
+-- 2) Inserir empréstimo se disponível
+INSERT INTO emprestimo (exemplar_id, usuario_id, data_emprestimo, data_prevista_devolucao, status)
+VALUES (
+  (SELECT id FROM exemplar WHERE codigo_exemplar = 'EX-0005'),
+  (SELECT id FROM usuario WHERE cpf = '44455566677'),
+  CURDATE(),
+  DATE_ADD(CURDATE(), INTERVAL 14 DAY),
+  'Ativo'
+);
+
+-- 3) Atualiza disponibilidade
+UPDATE exemplar SET disponivel = FALSE WHERE codigo_exemplar = 'EX-0005';
+
 COMMIT;
