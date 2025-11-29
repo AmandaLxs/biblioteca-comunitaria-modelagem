0269d08b392eb12187bea70aa88f8f9d74116730
Amanda Lima <amandalima.181@icloud.com>
2025-11-29 16:30:24 -0300

    V1 - DELETE examples (4 deletes)

--- sql/05_delete_commands.sql
+++ sql/05_delete_commands.sql
@@ -1,16 +1,20 @@
 -- 05_delete_commands.sql
 USE biblioteca_comunitaria;
 
--- 1) Remover usuário inativo e sem empréstimos (exemplo)
+-- DELETE 1: Remover usuário teste sem empréstimos
 DELETE FROM usuario
-WHERE id = (SELECT id FROM usuario WHERE cpf = '00000000000' LIMIT 1);
+WHERE cpf = '00000000000' 
+  AND id NOT IN (SELECT usuario_id FROM emprestimo);
 
--- 2) Excluir exemplar danificado (apenas se não estiver emprestado)
+-- DELETE 2: Excluir exemplar danificado (apenas se não estiver emprestado)
 DELETE FROM exemplar
-WHERE id = (SELECT ex.id FROM exemplar ex
-            LEFT JOIN emprestimo em ON em.exemplar_id = ex.id AND em.data_devolucao_real IS NULL
-            WHERE ex.codigo_exemplar = 'EX-9999' AND em.id IS NULL LIMIT 1);
+WHERE codigo_exemplar = 'EX-9999'
+  AND id NOT IN (SELECT exemplar_id FROM emprestimo WHERE data_devolucao_real IS NULL);
 
--- 3) Excluir livro sem exemplares (limpeza)
+-- DELETE 3: Excluir livros sem exemplares (limpeza)
 DELETE FROM livro
 WHERE id NOT IN (SELECT DISTINCT livro_id FROM exemplar);
+
+-- DELETE 4: Excluir multas pagas antigas (limpeza condicional)
+DELETE FROM multa
+WHERE paga = TRUE AND data_emissao < DATE_SUB(CURDATE(), INTERVAL 365 DAY);
